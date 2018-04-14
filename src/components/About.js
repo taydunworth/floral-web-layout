60b073e3e76fd04ddcdfc400ee24487c46e24011
Taylor Cox <taylorashtoncox@gmail.com>
2018-04-14 10:56:00 -0400

    Updated content and styling of About component

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,24 @@ import React, { Component } from 'react';
 class About extends Component {
   render() {
     return (
-      <p>About</p>
+      <div className="about">
+        <img src="/images/about.jpg" className="about-image" alt="About Image"/>
+        <div className="about-copy">
+          <h2>About.</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            congue, sapien at sagittis malesuada, felis velit laoreet eros,
+            elementum bibendum enim lectus et turpis. Aliquam aliquam nibh
+            eget tellus volutpat, quis pharetra nulla finibus. Integer
+            lobortis nunc mi, vel eleifend turpis aliquam nec.
+          </p>
+          <p>
+            Fusce ut orci in lorem ultricies maximus. Morbi vitae pretium
+            ipsum. In a volutpat tortor, et ultrices libero. Phasellus at
+            tincidunt massa, a semper nulla.
+          </p>
+        </div>
+      </div>
     );
   }
 }
