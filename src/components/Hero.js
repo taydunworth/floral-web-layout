8dbe637fca405e6d601cf64ad2345bc07f75d090
Taylor Cox <taylorashtoncox@gmail.com>
2018-04-13 22:14:10 -0400

    Updated hero to have copy with proper positioning for desktop and added hero image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class Hero extends Component {
   render() {
     return (
-      <p>Hero</p>
+      <div className="hero">
+        <h1>Lorem ipsum<br/> dolor.</h1>
+      </div>
     );
   }
 }
