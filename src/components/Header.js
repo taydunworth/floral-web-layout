97d8cd4f596589318fe78ee53eae39e73d3b0723
Taylor Cox <taylorashtoncox@gmail.com>
2018-04-13 22:00:47 -0400

    Added fonts, added nav content and styling to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <p>Header</p>
+      <div className="header">
+        <ul>
+          <li><a href="#About">About</a></li>
+          <li><a href="#Work">Work</a></li>
+          <li><a href="#Contact">Contact</a></li>
+        </ul>
+      </div>
     );
   }
 }
