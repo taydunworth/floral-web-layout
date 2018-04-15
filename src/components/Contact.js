d2da4a0718bd89a75a155aaaf49431d78abf7366
Taylor Cox <taylorashtoncox@gmail.com>
2018-04-14 23:51:12 -0400

    Started HTML and CSS for Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,22 @@ import React, { Component } from 'react';
 class Contact extends Component {
   render() {
     return (
-      <p>Contact</p>
+      <div className="contact">
+        <div className="left">
+          <h2>Contact.</h2>
+          <div className="contact-box">
+            <input type="text" className="one-half"/>
+            <input type="text" className="one-half"/>
+            <input type="text" className="full"/>
+            <input type="text" className="full"/>
+            <textarea></textarea>
+          </div>
+        </div>
+        <div className="right">
+          <img src="/images/contact.jpg" alt="Bouquet" />
+        </div>
+
+      </div>
     );
   }
 }
